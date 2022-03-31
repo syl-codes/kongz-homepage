afa73e9e5c9a68416615493d00da61fb93752f75
raon ysh <sayas@naver.com>
2022-03-31 18:59:10 +0900

    거버넌스

--- _js/bootstrap.modal.remote.js
+++ _js/bootstrap.modal.remote.js
@@ -1,15 +1,25 @@
 $(document).on('show.bs.modal', '.modal', function (e) {
-	var param = $(e.relatedTarget).data('param');
-	var modalId = $(this).attr('id');
-	var modalUrl = $(document).find("[data-target='#"+modalId+"']").attr('href');
-	if(!param){	var param =""; }
-	$("#"+modalId).find(".modal-content").load(modalUrl+param);
+	if(!$(this).hasClass("inner")){
+		var param = $(e.relatedTarget).data('param');
+		var modalId = $(this).attr('id');
+		var modalUrl = $(document).find("[data-target='#"+modalId+"']").attr('href');
+		if(!param){	var param =""; }
+		$("#"+modalId).find(".modal-content").load(modalUrl+param);
 
-		// var modalId = $(this).attr('id');
-		// var modalUrl = $(document).find("[data-target='#"+modalId+"']").attr('href');
-		// console.log(modalId+"==="+modalUrl);
-		// $("#"+modalId).find(".modal-content").load(modalUrl);
+			// var modalId = $(this).attr('id');
+			// var modalUrl = $(document).find("[data-target='#"+modalId+"']").attr('href');
+			// console.log(modalId+"==="+modalUrl);
+			// $("#"+modalId).find(".modal-content").load(modalUrl);
+	}
+	
 });
 $(document).on('hidden.bs.modal', '.modal', function () {
-    $(this).find(".modal-content").empty();
+	if(!$(this).hasClass("inner")){
+		$(this).find(".modal-content").empty();
+	}
+	$('.modal:visible').length && $(document.body).addClass('modal-open');
 });
+
+
+
+
